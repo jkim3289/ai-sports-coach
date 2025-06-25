8d1ce73c441617d498dfb7deb42453b79477347b
Jehun Kim <wpgns4587@gmail.com>
2025-06-25 18:50:14 -0400

    detect person, tennis racket, and sports ball

--- HowIsYourSwing/main.py
+++ HowIsYourSwing/main.py
@@ -6,8 +6,10 @@ import os
 model = YOLO("yolov8n.pt")  # Try yolov8s.pt for better accuracy
 
 # Run inference and save results
-results = model("/Users/jehunkim/Desktop/Summer 2025/ai-sports-coach/videos/test2.MOV", save=True)
+results = model("/Users/jehunkim/Desktop/Summer 2025/ai-sports-coach/videos/test1.MOV", classes=[0, 32, 38], save=True)
 
 # Find where results are saved
 save_dir = results[0].save_dir
 print(f"Output saved at: {save_dir}")
+
+print(model.names)
